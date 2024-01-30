cfeb2af9992c9a08de6ae03e1400b194248b2e1f
Canyon Smith <canyonfsmith@gmail.com>
2024-01-30 12:22:30 -0700

    Create hooks.js

---
+++ hooks.js
@@ -0,0 +1,12 @@
+export const HOOKS = [
+  'What if you could (___desire___) without (___pain____)?',
+  'You wanna know why (__common vehicle people use to get desire__) doesn’t work when you’re trying to (___desire___)?',
+  'Worried about (___pain____)?',
+  'Here are the Top Questions I get about how to  (___desire___) ?',
+  '(__X number__) ways to get (___desire___) in (__realistic, but surprising time frame__).',
+  '(__X number__) things you should NEVER do if you want to  (___desire___).',
+  'The #1 (__topic of interest/niche__) mistake people make that keeps them from (___desire___) is (__super relevant and common mistake that might surprise most people__).',
+  'Here’s the top (__X number__) questions I hear beginner (__ideal market avatar__) trying to  (___desire___).',
+  '(__everyday person like a mom, college student, single guy, etc.__) reveals little known trick used by (__well known experts/industries__)',
+  'Here’s how (__ideal market avatar__) are (___desire___)  without (___pain____).'
+];
