24b85bce90bd81f45fc52a00947eb8d05ad24b43
Canyon Smith <canyonfsmith@gmail.com>
2024-01-30 12:32:38 -0700

    Refactor JSX files and update images

--- hooks.js
+++ hooks.js
@@ -1,12 +1,13 @@
+
 export const HOOKS = [
-  'What if you could (___desire___) without (___pain____)?',
-  'You wanna know why (__common vehicle people use to get desire__) doesn’t work when you’re trying to (___desire___)?',
-  'Worried about (___pain____)?',
-  'Here are the Top Questions I get about how to  (___desire___) ?',
-  '(__X number__) ways to get (___desire___) in (__realistic, but surprising time frame__).',
-  '(__X number__) things you should NEVER do if you want to  (___desire___).',
-  'The #1 (__topic of interest/niche__) mistake people make that keeps them from (___desire___) is (__super relevant and common mistake that might surprise most people__).',
-  'Here’s the top (__X number__) questions I hear beginner (__ideal market avatar__) trying to  (___desire___).',
-  '(__everyday person like a mom, college student, single guy, etc.__) reveals little known trick used by (__well known experts/industries__)',
-  'Here’s how (__ideal market avatar__) are (___desire___)  without (___pain____).'
+  'What if you could create stunning tattoos without any prior experience?',
+  'You wanna know why traditional art classes don’t work when you’re trying to learn tattooing?',
+  'Worried about making mistakes on skin?',
+  'Here are the Top Questions I get about how to master tattooing?',
+  '5 ways to get proficient in tattooing in just 2 weeks.',
+  '3 things you should NEVER do if you want to excel in tattooing.',
+  'The #1 tattooing mistake people make that keeps them from mastering it is not understanding skin types.',
+  'Here’s the top 5 questions I hear beginner tattoo enthusiasts trying to master tattooing.',
+  'Single mom reveals little known trick used by professional tattoo artists',
+  'Here’s how tattoo enthusiasts are mastering the art without causing skin damage.'
 ];
