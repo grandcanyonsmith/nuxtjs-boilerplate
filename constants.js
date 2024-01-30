434e7751b231539ca940310f5bc2347bb1a47998
Canyon Smith <canyonfsmith@gmail.com>
2024-01-30 12:19:43 -0700

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,10 @@
+export const desire = 'your desire text';
+export const pain = 'your pain text';
+export const commonVehicle = 'common vehicle text';
+export const surprisingTimeFrame = 'surprising time frame text';
+export const xNumber = 'x number text';
+export const topicOfInterest = 'topic of interest text';
+export const commonMistake = 'common mistake text';
+export const marketAvatar = 'market avatar text';
+export const everydayPerson = 'everyday person text';
+export const wellKnownExperts = 'well known experts text';
