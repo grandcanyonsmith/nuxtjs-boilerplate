fddbf460d0eeea005c2a9bd8a3bd87284bc84ae6
Canyon Smith <canyonfsmith@gmail.com>
2024-01-30 12:43:44 -0700

    Refactor JSX files and update images

--- hooks.js
+++ hooks.js
@@ -1,12 +1,13 @@
+
 export const HOOKS = [
-  'What if you could (___desire___) without (___pain____)?',
-  'You wanna know why (__common vehicle people use to get desire__) doesn’t work when you’re trying to (___desire___)?',
-  'Worried about (___pain____)?',
-  'Here are the Top Questions I get about how to  (___desire___) ?',
-  '(__X number__) ways to get (___desire___) in (__realistic, but surprising time frame__).',
-  '(__X number__) things you should NEVER do if you want to  (___desire___).',
-  'The #1 (__topic of interest/niche__) mistake people make that keeps them from (___desire___) is (__super relevant and common mistake that might surprise most people__).',
-  'Here’s the top (__X number__) questions I hear beginner (__ideal market avatar__) trying to  (___desire___).',
-  '(__everyday person like a mom, college student, single guy, etc.__) reveals little known trick used by (__well known experts/industries__)',
-  'Here’s how (__ideal market avatar__) are (___desire___)  without (___pain____).'
+  'What if you could become a full-time filmmaker without quitting your day job?',
+  'You wanna know why film school doesn’t work when you’re trying to become a full-time filmmaker?',
+  'Worried about lack of experience in filmmaking?',
+  'Here are the Top Questions I get about how to become a full-time filmmaker?',
+  '5 ways to become a full-time filmmaker in less than a year.',
+  '3 things you should NEVER do if you want to become a full-time filmmaker.',
+  'The #1 filmmaking mistake people make that keeps them from becoming a full-time filmmaker is not understanding the business side of the industry.',
+  'Here’s the top 5 questions I hear beginner filmmakers trying to become full-time.',
+  'Single mom reveals little known trick used by Hollywood directors',
+  'Here’s how aspiring filmmakers are becoming full-time without going to film school.'
 ];
