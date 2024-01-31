39d429f7185ef52b073a1690696b7ca573534b80
Canyon Smith <canyonfsmith@gmail.com>
2024-01-30 17:12:18 -0700

    Refactor JSX files and update images

--- hooks.js
+++ hooks.js
@@ -1,12 +1,13 @@
+
 export const HOOKS = [
-  'What if you could (___desire___) without (___pain____)?',
-  'You wanna know why (__common vehicle people use to get desire__) doesn’t work when you’re trying to (___desire___)?',
-  'Worried about (___pain____)?',
-  'Here are the Top Questions I get about how to  (___desire___) ?',
-  '(__X number__) ways to get (___desire___) in (__realistic, but surprising time frame__).',
-  '(__X number__) things you should NEVER do if you want to  (___desire___).',
-  'The #1 (__topic of interest/niche__) mistake people make that keeps them from (___desire___) is (__super relevant and common mistake that might surprise most people__).',
-  'Here’s the top (__X number__) questions I hear beginner (__ideal market avatar__) trying to  (___desire___).',
-  '(__everyday person like a mom, college student, single guy, etc.__) reveals little known trick used by (__well known experts/industries__)',
-  'Here’s how (__ideal market avatar__) are (___desire___)  without (___pain____).'
+  'What if you could catch more ducks without spending hours in the cold?',
+  'You wanna know why traditional hunting methods don’t work when you’re trying to catch more ducks?',
+  'Worried about not being able to attract ducks?',
+  'Here are the Top Questions I get about how to catch more ducks?',
+  '5 ways to catch more ducks in less than a week.',
+  '3 things you should NEVER do if you want to catch more ducks.',
+  'The #1 hunting mistake people make that keeps them from catching more ducks is not using the right bait.',
+  'Here’s the top 5 questions I hear beginner hunters trying to catch more ducks.',
+  'Avid hunter reveals little known trick used by professional duck hunters',
+  'Here’s how successful hunters are catching more ducks without spending hours in the cold.'
 ];
